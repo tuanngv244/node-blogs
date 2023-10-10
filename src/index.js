0d1d8fb25610c306b6f6c830dd1e7487ac4e3899
Tuan Nguyen <tuan.nv1@isofhcare.com>
2023-10-10 16:34:58 +0700

    33

--- src/index.js
+++ src/index.js
@@ -7,22 +7,22 @@ const route = require('./routes');
 const port = 3000;
 
 // Using data resources static
-                      app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //
-   app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // HTTP logger
-                                                                                                 app.use(morgan('combined'));
+    app.use(morgan('combined'));
 
 // Template
-app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
+  app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
 route(app);
 
-                                  app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
