199664de1c8ca2c671831bea7853057a6edcdf4e
Tuan Nguyen <tuan.nv1@isofhcare.com>
2023-10-10 16:28:06 +0700

    d

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const port = 3000;
                       app.use(express.static(path.join(__dirname, 'public')));
 
 //
-app.use(express.urlencoded({ extended: true }));
+   app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // HTTP logger
                                                                                                  app.use(morgan('combined'));
