fd89847cf85d658e90ddcd9809e7f0b2abbe1fa2
Tuan Nguyen <tuan.nv1@isofhcare.com>
2023-10-10 16:24:56 +0700

    run hooks husky

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const path = require("path");
-const morgan = require("morgan");
-const handlebars = require("express-handlebars");
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
 const app = express();
-const route = require("./routes");
+const route = require('./routes');
 const port = 3000;
 
 // Using data resources static
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // HTTP logger
-     app.use(morgan("combined"));
+                                                                                                 app.use(morgan('combined'));
 
 // Template
-app.engine("hbs", handlebars.engine({ extname: ".hbs" }));
-      app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
 route(app);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+                                  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,21 @@
-const newsRouter = require("./news");
-const siteRouter = require("./site");
+const newsRouter = require('./news');
+const siteRouter = require('./site');
 
 const RoutePaths = {
-  HOME: "/",
-  NEWS: "/news",
-  SEARCH: "/search",
+    HOME: '/',
+    NEWS: '/news',
+    SEARCH: '/search',
 };
 
 function route(app) {
-  app.use(RoutePaths.NEWS, newsRouter);
-  app.use(RoutePaths.HOME, siteRouter);
+    app.use(RoutePaths.NEWS, newsRouter);
+    app.use(RoutePaths.HOME, siteRouter);
 
-  app.post("/search", (req, res) => {
-    //req.query ----> how to get query parameters from API url
-    console.log(req.body);
-    return res.render("search");
-  });
+    app.post('/search', (req, res) => {
+        //req.query ----> how to get query parameters from API url
+        console.log(req.body);
+        return res.render('search');
+    });
 }
 
 module.exports = route;
